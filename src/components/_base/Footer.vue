a70550a79527eea69baec0e4215b5da4b956f271
jundasharin <harisjundan@gmail.com>
2021-01-18 22:21:03 +0700

    create footer

---
+++ src/components/_base/Footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <footer>
+    <b-container class="footer-container">
+      <b-row class="d-flex justify-content-between">
+        <b-col class="footer-left">
+          <b-row>
+            <b-col class="d-flex align-item-center"
+              ><img src="../../assets/icons/coffee-logo.svg" />
+              <p class="my-auto footer-brand pl-2">Coffee Shop</p></b-col
+            >
+          </b-row>
+          <p class="footer-desc">
+            Coffee Shop is a store that sells some good meals, and especially
+            coffee. We provide high quality beans.
+          </p>
+          <div class="d-flex">
+            <div class="icons-socmed">
+              <img
+                style="height: 20px; width: 20px"
+                src="../../assets/icons/facebook.svg"
+                alt=""
+              />
+            </div>
+            <div class="icons-socmed">
+              <img
+                style="height: 20px; width: 20px"
+                src="../../assets/icons/twitter.svg"
+                alt=""
+              />
+            </div>
+            <div class="icons-socmed">
+              <img
+                style="height: 20px; width: 20px"
+                src="../../assets/icons/instagram.svg"
+                alt=""
+              />
+            </div>
+          </div>
+          <p class="cs-footer">&copy; Coffee Shop 2020</p>
+        </b-col>
+        <b-col class="footer-product" sm>
+          <p class="footer-product-title mb-4" style="font-weight: bold;">
+            Product
+          </p>
+          <p>Download</p>
+          <p>Pricing</p>
+          <p>Location</p>
+          <p>Countries</p>
+          <p>Blog</p>
+        </b-col>
+        <b-col class="footer-engage" sm>
+          <p class="footer-engage-title mb-4" style="font-weight: bold;">
+            Engage
+          </p>
+          <p>Coffee Shop</p>
+          <p>FAQ</p>
+          <p>About Us</p>
+          <p>Privacy Policy</p>
+          <p>Terms Of Services</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <Navbar />
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>My name is {{ name }}</h1>
+    <Footer />
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue"
 import Navbar from "../components/_base/Navbar"
+import Footer from "../components/_base/Footer"
 
 export default {
   name: "Product",
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
   data() {
     return {
